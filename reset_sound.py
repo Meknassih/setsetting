aa24bb5ad090e2d86f4c219f340b95746b8360d1
Hamza El Meknassi <elmeknassi.h@gmail.com>
2021-01-24 14:53:39 +0100

    Use while to locate objects on screen
    Sound panel button is located instead of hardcoded

--- reset_sound.py
+++ reset_sound.py
@@ -16,45 +16,63 @@ pyautogui.rightClick(sysTrayIconCoords['x'], sysTrayIconCoords['y'])
 pyautogui.leftClick(soundsMenuCoords['x'], soundsMenuCoords['y'])
 
 # Go to Playback devices tab
-time.sleep(ACTION_TIME_SEC)
 playbackTabCoords = pyautogui.locateCenterOnScreen('playback_tab.png')
-if (playbackTabCoords == None):
-    print('Could not find Playback tab, exit...')
-    sys.exit(1)
+while (playbackTabCoords == None):
+    playbackTabCoords = pyautogui.locateCenterOnScreen('playback_tab.png')
+    # print('Could not find Playback tab, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(playbackTabCoords[0], playbackTabCoords[1])
+
+# DEBUG
+# input("Press Enter to continue...")
 
 # Open Speakers device
 deviceCoords = pyautogui.locateCenterOnScreen('audio_device.png')
-if (deviceCoords == None):
-    print('Could not find Device entry, exit...')
-    sys.exit(1)
+while (deviceCoords == None):
+    deviceCoords = pyautogui.locateCenterOnScreen('audio_device.png')
+    # print('Could not find Device entry, exit...')
+    # sys.exit(1)
 pyautogui.doubleClick(deviceCoords[0], deviceCoords[1])
 
+# DEBUG
+# input("Press Enter to continue...")
+
 # Go to Advanced settings tab
-time.sleep(ACTION_TIME_SEC)
 advancedTabCoords = pyautogui.locateCenterOnScreen('advanced_tab.png')
-if (advancedTabCoords == None):
-    print('Could not find Advanced tab, exit...')
-    sys.exit(1)
+while (advancedTabCoords == None):
+    advancedTabCoords = pyautogui.locateCenterOnScreen('advanced_tab.png')
+    # print('Could not find Advanced tab, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(advancedTabCoords[0], advancedTabCoords[1])
+
+# DEBUG
+# input("Press Enter to continue...")
 
 # Click on audio format dropdown
 formatDropdownCoords = pyautogui.locateCenterOnScreen('dropdown.png')
-if (formatDropdownCoords == None):
-    print('Could not find Format dropdown, exit...')
-    sys.exit(1)
+while (formatDropdownCoords == None):
+    formatDropdownCoords = pyautogui.locateCenterOnScreen('dropdown.png')
+    # print('Could not find Format dropdown, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(formatDropdownCoords[0], formatDropdownCoords[1])
 
+# DEBUG
+# input("Press Enter to continue...")
+
 # Select Studio quality format
-pyautogui.moveRel(studioQualityOffset['x'], studioQualityOffset['y'], .2)
+pyautogui.moveRel(studioQualityOffset['x'], studioQualityOffset['y'])
 pyautogui.leftClick()
 
 # Test the audio
 testButtonCoords = pyautogui.locateCenterOnScreen('test_button.png')
-if (testButtonCoords == None):
-    print('Could not find Test button, exit...')
-    sys.exit(1)
+while (testButtonCoords == None):
+    testButtonCoords = pyautogui.locateCenterOnScreen('test_button.png')
+    # print('Could not find Test button, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(testButtonCoords[0], testButtonCoords[1])
+
+# DEBUG
+# input("Press Enter to continue...")
 
 # Close warning dialog if it pops up
 time.sleep(ACTION_TIME_SEC/2)
@@ -64,30 +82,43 @@ if (warningYesCoords == None):
 else:
     pyautogui.leftClick(warningYesCoords[0], warningYesCoords[1])
 
+# DEBUG
+# input("Press Enter to continue...")
+
 # Stop the audio test
 # stopButtonCoords = pyautogui.locateCenterOnScreen('stop_button.png')
 pyautogui.leftClick(testButtonCoords[0], testButtonCoords[1])
 
+# DEBUG
+# input("Press Enter to continue...")
+
 # Click on audio format dropdown
-pyautogui.leftClick(formatDropdownCoords[0], formatDropdownCoords[1], 0, .5)
+pyautogui.leftClick(formatDropdownCoords[0], formatDropdownCoords[1])
+
+# DEBUG
+# input("Press Enter to continue...")
 
 # Select DVD quality format
-pyautogui.moveRel(dvdQualityOffset['x'], dvdQualityOffset['y'], .2)
+pyautogui.moveRel(dvdQualityOffset['x'], dvdQualityOffset['y'])
 pyautogui.leftClick()
 
 # Close second panel
 okButtonCoords = pyautogui.locateCenterOnScreen('ok_button.png')
-if (okButtonCoords == None):
-    print('Could not find OK button, exit...')
-    sys.exit(1)
+while (okButtonCoords == None):
+    okButtonCoords = pyautogui.locateCenterOnScreen('ok_button.png')
+    # print('Could not find OK button, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(okButtonCoords[0], okButtonCoords[1])
 
+# DEBUG
+# input("Press Enter to continue...")
+
 # Close first panel
-time.sleep(ACTION_TIME_SEC/2)
 okButtonCoords = pyautogui.locateCenterOnScreen('ok_button.png')
-if (okButtonCoords == None):
-    print('Could not find OK button, exit...')
-    sys.exit(1)
+while (okButtonCoords == None):
+    okButtonCoords = pyautogui.locateCenterOnScreen('ok_button.png')
+    # print('Could not find OK button, exit...')
+    # sys.exit(1)
 pyautogui.leftClick(okButtonCoords[0], okButtonCoords[1])
 
 print('Done resetting sound format.')
